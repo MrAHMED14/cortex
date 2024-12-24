bc824325095378aaf1ba75e8dde3e9804b12517f
MrAHMED <chikhaoui084@gmail.com>
2024-12-24 07:05:12 -0800

    refactor: change font weight for 404 error message in NotFound component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <div className="h-screen w-full flex flex-col gap-10 items-center justify-center">
-      <p className="font-bold text-lg">
+      <p className="font-medium text-lg">
         404 | Could not find requested resource
       </p>
       <Link href="/" className={buttonVariants({})}>
