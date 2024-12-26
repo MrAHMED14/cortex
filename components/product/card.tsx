a1aac70454aef0a907e997534ad3f4c769afebdb
MrAHMED <chikhaoui084@gmail.com>
2024-12-26 14:31:21 -0800

    refactor: improve ProductCard layout and styling for better responsiveness

--- components/product/card.tsx
+++ components/product/card.tsx
@@ -39,22 +39,22 @@ export default function ProductCard({
         className
       )}
     >
-      <div className="w-full h-full flex items-center justify-center">
-        <div className="w-full h-full flex items-center justify-center pt-3">
-          {/* Image */}
+      {/* Image */}
+      <div className="w-full h-full flex items-center justify-center pt-3">
+        <div className="w-40 aspect-square overflow-hidden rounded bg-gray-100 scale-[.97]">
           <Image
             src={img[0]}
             width={150}
             height={150}
             alt={title}
-            className="w-[160px] h-[160px] object-cover object-center rounded scale-[.97]"
+            className="w-full h-full object-cover object-center"
           />
-          {/* <div className="w-[160px] h-[160px] {w-[180px]} {h-40} bg-gray-300/80 rounded-lg animate-pulse" /> */}
         </div>
       </div>
+
       <div className="w-full h-full">
         <div className="w-full h-full flex flex-col items-center justify-center">
-          <div className="w-full h-full px-4 mt-3">
+          <div className="w-full h-full px-4 mt-2">
             {/* Title */}
             <p className="text-stone-950 font-medium text-lg line-clamp-2 h-fit">
               {title}
@@ -110,11 +110,6 @@ export default function ProductCard({
         {discountPercentage && (
           <Badge className="uppercase bg-yellow-400 hover:bg-yellow-300 text-black">
             -{discountPercentage.toFixed(0)}%
-          </Badge>
-        )}
-        {false && (
-          <Badge className="uppercase bg-violet-500 hover:bg-violet-400">
-            Hot deals
           </Badge>
         )}
       </div>
