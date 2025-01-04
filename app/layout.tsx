27eadd8917e018309e36cd58ff879c638a0e913f
MrAHMED <chikhaoui084@gmail.com>
2025-01-03 16:25:17 -0800

    feat: enhance metadata with Google verification and seo keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,41 @@ import "./globals.css"
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL as string),
+  verification: {
+    google: "jb87j8BcZ8ZP9S4QgE3e50Q6bUjMc0a1AGVsjz5lANY",
+  },
   title: {
     default: "Cortex Store",
     template: "%s - Cortex Store",
   },
   description:
     "The ultimate destination for high-quality tech products and the latest gadgets, offering exceptional prices and top-notch service.",
-
   twitter: {
     card: "summary_large_image",
   },
+  keywords: [
+    "cortex",
+    "cortex website",
+    "cortex store",
+    "cortex-store",
+    "cortex store website",
+    "e-commerce",
+    "online",
+    "tech",
+    "gadgets",
+    "electronics",
+    "store",
+    "shop",
+    "products",
+    "buy",
+    "nextjs",
+    "react",
+    "tailwindcss",
+    "typescript",
+    "chikhaoui ahmed",
+    "chikhaoui",
+    "ahmed",
+  ],
 }
 
 export default async function RootLayout({
